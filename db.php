84f21199b47dfb2e2ffc2532931b785dc301c9c2
WeslleyHektor <weslleycaua000@gmail.com>
2024-09-27 22:11:05 -0300

    Projeto Acai Tribus

---
+++ db.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "acai_db2";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+?>
+<html>
+
+<!-- BANCO DE DADOS
+
+CREATE DATABASE acai;
+
+USE acai;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+    cargo ENUM('funcionario', 'gestor') NOT NULL,
+    aprovado TINYINT DEFAULT 0
+);
+
+SELECT * FROM usuarios;
+
+DROP DATABASE acai;-->
+
+</html>
